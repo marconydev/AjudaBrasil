1096acde31c4c3ccff4cc66d954a2d6e2c789d38
Marcony Junior <marconydev@gmail.com>
2020-05-24 12:57:55 -0300

    App AJudaBrasil
    Projeto para consulta de leis aprovadas pelo governo

---
+++ app/src/main/java/com/JoaoVictor/ajudabrasil/Proposicao.kt
@@ -0,0 +1,8 @@
+package com.JoaoVictor.ajudabrasil
+
+//Modelo
+data class Proposicao(var id: Int,
+                      var uri: String,
+                        var siglaTipo: String,
+                        var ano: Int,
+                        var ementa: String)
